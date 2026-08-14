9ebef3b2ab8798f406de7fd0330a3484cd43374e
eduardo.goes <66528152+rodrigues730@users.noreply.github.com>
2026-08-14 01:15:26 -0300

    Implement service worker with caching strategies

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,69 @@
+const CACHE_VERSION = 'iron-v3-images';
 
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.webmanifest',
+    './icons/icon-192.png',
+    './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_VERSION)
+            .then(cache => cache.addAll(APP_SHELL))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys =>
+                Promise.all(
+                    keys
+                        .filter(key => key !== CACHE_VERSION)
+                        .map(key => caches.delete(key))
+                )
+            )
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+
+    if (request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(request)
+            .then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(request)
+                    .then(networkResponse => {
+                        if (
+                            networkResponse &&
+                            networkResponse.status === 200 &&
+                            networkResponse.type !== 'opaque'
+                        ) {
+                            const responseClone = networkResponse.clone();
+
+                            caches.open(CACHE_VERSION)
+                                .then(cache => {
+                                    cache.put(request, responseClone);
+                                });
+                        }
+
+                        return networkResponse;
+                    })
+                    .catch(() => {
+                        return caches.match('./index.html');
+                    });
+            })
+    );
+});
